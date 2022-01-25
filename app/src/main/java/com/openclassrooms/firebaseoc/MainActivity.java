58095251d28e07a0cdb314fde76a654f12883730
MOHASSEB <abdelrahman.diaaa@gmail.com>
2022-01-25 20:37:48 +0100

    authentification

--- app/src/main/java/com/openclassrooms/firebaseoc/MainActivity.java
+++ app/src/main/java/com/openclassrooms/firebaseoc/MainActivity.java
@@ -2,11 +2,18 @@ package com.openclassrooms.firebaseoc;
 
 import androidx.annotation.Nullable;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.firebase.ui.auth.AuthUI;
 import com.openclassrooms.firebaseoc.databinding.ActivityMainBinding;
 
+import java.util.Collections;
+import java.util.List;
+
 public class MainActivity extends BaseActivity<ActivityMainBinding> {
+
+    private static final int RC_SIGN_IN = 123;
 
     @Override
     ActivityMainBinding getViewBinding() {
@@ -16,6 +23,50 @@ public class MainActivity extends BaseActivity<ActivityMainBinding> {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setupListeners();
     }
+    private void setupListeners(){
+        // Login Button
+        binding.loginButton.setOnClickListener(view -> {
+            startSignInActivity();
+        });
+
+    }
+
+    private void startSignInActivity(){
+
+        // Choose authentication providers
+        // type of connections (here we add connect by email)
+        List<AuthUI.IdpConfig> providers =
+                Collections.singletonList(new AuthUI.IdpConfig.EmailBuilder().build());
+
+        // Launch the activity and wait for the return to see if the authentification is success or no due to start
+        // activity for Result
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setTheme(R.style.LoginTheme)
+                        .setAvailableProviders(providers)
+                        .setIsSmartLockEnabled(false, true)
+                        .setLogo(R.drawable.ic_logo_auth)
+                        .build(),
+                RC_SIGN_IN);
+
+    }
+
+    // in this method we have a callback to the function used when trying to sign in
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        this.handleResponseAfterSignIn(requestCode, resultCode, data);
+    }
+    // Show Snack Bar with a message
+    private void showSnackBar( String message){
+        Snackbar.make(binding.mainLayout, message, Snackbar.LENGTH_SHORT).show();
+    }
+
+
+
+
 
 }
